b29a75765cb6bd6ff0640432bc8c719444bae77f
Vetka921 <vetka921@gmail.com>
2018-06-14 09:58:24 +0700

    us

---
+++ callback_u/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', 'bbase/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '9964d4df'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '4e3d150783864e6e10ea6c9ea0d1602cafd10dbec20a9f3781b93185e4000f5c7026e837122a8e4e41e66'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '65deaee2-c606-4577-9ef2-6cb44dc84c34'); //Ключ для доступа к Yandex Speech Kit
---
+++ callback_u/index.php
@@ -0,0 +1,124 @@
+<?php
+/*
+_callback_response('9964d4df');
+exit();
+*/
+define('CALLBACK_API_EVENT_CONFIRMATION', 'confirmation');
+define('CALLBACK_API_EVENT_MESSAGE_NEW', 'message_new');
+
+require_once 'config.php';
+require_once 'global.php';
+
+require_once 'api/vk_api.php';
+require_once 'api/yandex_api.php';
+
+require_once 'bot/bot.php';
+
+if (isset($_GET['uid'])) {
+  $uid=$_GET['uid'];
+}
+
+
+
+function setInterval($func = null, $interval = 0, $times = 0){
+  if( ($func == null) || (!function_exists($func)) ){
+    throw new Exception('We need a valid function.');
+  }
+
+  /*
+  usleep delays execution by the given number of microseconds.
+  JavaScript setInterval uses milliseconds. microsecond = one 
+  millionth of a second. millisecond = 1/1000 of a second.
+  Multiplying $interval by 1000 to mimic JS.
+  */
+
+
+  $seconds = $interval * 1000;
+
+  /*
+  If $times > 0, we will execute the number of times specified.
+  Otherwise, we will execute until the client aborts the script.
+  */
+
+  if($times > 0){
+    
+    $i = 0;
+    
+    while($i < $times){
+        call_user_func($func);
+        $i++;
+        usleep( $seconds );
+    }
+  } else {
+    
+    while(true){
+        call_user_func($func); // Call the function you've defined.
+        usleep( $seconds );
+    }
+  }
+}
+$attachments=new array;
+function doit(){
+  vkApi_messagesSend('222194683', "Hi", $attachments);
+}
+
+setInterval('doit', 1000, 5); // Invoke every second, up to 100 times.
+
+
+
+
+
+
+/*
+callback_handleEvent();
+
+function callback_handleEvent() {
+  $event = _callback_getEvent();
+
+  try {
+    switch ($event['type']) {
+      //Подтверждение сервера
+      case CALLBACK_API_EVENT_CONFIRMATION:
+        _callback_handleConfirmation();
+        break;
+
+      //Получение нового сообщения
+      case CALLBACK_API_EVENT_MESSAGE_NEW:
+        _callback_handleMessageNew($event['object']);
+        break;
+
+      default:
+        _callback_response('Unsupported event');
+        break;
+    }
+  } catch (Exception $e) {
+    log_error($e);
+  }
+
+  _callback_okResponse();
+}
+
+function _callback_getEvent() {
+  return json_decode(file_get_contents('php://input'), true);
+}
+
+function _callback_handleConfirmation() {
+  _callback_response(CALLBACK_API_CONFIRMATION_TOKEN);
+}
+
+function _callback_handleMessageNew($data) {
+  $user_id = $data['user_id'];
+  bot_sendMessage($user_id);
+  _callback_okResponse();
+}
+
+function _callback_okResponse() {
+  _callback_response('ok');
+}
+
+function _callback_response($data) {
+  echo $data;
+  exit();
+}
+*/
+
