27ea01d4b584e79e45782306434fa135f7db4f2d
Vetka921 <vetka921@gmail.com>
2018-06-14 07:54:03 +0700

    First_secretly_upload

---
+++ callback/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', 'bbase/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '9964d4df'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '9423339e8756d4ef52eebe72ee7e152707bce0a81629bb8473b9453a9346f98d2ad232a5004db80cf5303'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '65deaee2-c606-4577-9ef2-6cb44dc84c34'); //Ключ для доступа к Yandex Speech Kit
